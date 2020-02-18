e6bced26349c947397eadc5e42c762cbfd5a71cc
Dan F-M <foreman.mackey@gmail.com>
2020-02-18 16:23:15 +1100

    adding environment and fixing typos

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-
+from collections import OrderedDict
 import ads
 
 
@@ -31,25 +31,28 @@ def parse_name(author_name):
     full_name = f"{first_name} {last_name}"
 
     # Check if it's a suffix or an actual name
-    #if re.search("([A-Z]\. ?)+", given_names):
+    # if re.search("([A-Z]\. ?)+", given_names):
     only_initials = len(given_names.replace(".", "").replace(" ", "")) <= 2
-    is_collaboration = "collaboration" in last_name.lower() \
-                     or "survey" in last_name.lower() \
-                     or "university" in last_name.lower()
-    
+    is_collaboration = (
+        "collaboration" in last_name.lower()
+        or "survey" in last_name.lower()
+        or "university" in last_name.lower()
+    )
+
     has_full_name = not any([is_collaboration, only_initials])
 
-    return OrderedDict([
-        ("first_name", first_name),
-        ("last_name", last_name),
-        ("given_names", given_names),
-        ("suffix", suffix),
-        ("full_name", full_name),
-        ("has_full_name", has_full_name),
-        ("only_initials", only_initials),
-        ("is_collaboration", is_collaboration)
-    ])
-
+    return OrderedDict(
+        [
+            ("first_name", first_name),
+            ("last_name", last_name),
+            ("given_names", given_names),
+            ("suffix", suffix),
+            ("full_name", full_name),
+            ("has_full_name", has_full_name),
+            ("only_initials", only_initials),
+            ("is_collaboration", is_collaboration),
+        ]
+    )
 
 
 def name_generator(author_name, **kwargs):
@@ -68,20 +71,23 @@ def name_generator(author_name, **kwargs):
     # Parse the input name
     parsed_author_name = parse_name(author_name)
 
-    search_kwds = dict(q=f"author:\"{author_name}\"",
-                       rows=rows, start=0, max_pages=max_pages,
-                       fl=["id", "author", "bibcode", "year", "aff"])
-
+    search_kwds = dict(
+        q=f'author:"{author_name}"',
+        rows=rows,
+        start=0,
+        max_pages=max_pages,
+        fl=["id", "author", "bibcode", "year", "aff"],
+    )
 
     # We want the generator to start yielding results back quickly.
     bibcodes = []
     first_author_bibcodes = []
 
     for j, article in enumerate(ads.SearchQuery(**search_kwds)):
-    
-        suggestion = dict(id=article.id,
-                          year=article.year,
-                          bibcode=article.bibcode)
+
+        suggestion = dict(
+            id=article.id, year=article.year, bibcode=article.bibcode
+        )
         bibcodes.append(article.bibcode)
 
         for k, (author, aff) in enumerate(zip(article.author, article.aff)):
@@ -90,24 +96,26 @@ def name_generator(author_name, **kwargs):
 
             # Does the first author match our author name?
             # TODO: Just checking by last name here. Consider doing things betterer.
-            if not k and parsed_author_name["last_name"] == parse_name(author)["last_name"]:
+            if (
+                not k
+                and parsed_author_name["last_name"]
+                == parse_name(author)["last_name"]
+            ):
                 first_author_bibcodes.append(article.bibcode)
 
     # Now let's do a similarity search on the first author bibcodes.
 
-
     raise StopIteration(f"that's all I got (from {j+1} articles)!")
 
 
-def bulk_similarity_search(bibcodes, batch_size=20, rows=20, max_pages=5, **kwargs):
+def bulk_similarity_search(
+    bibcodes, batch_size=20, rows=20, max_pages=5, **kwargs
+):
     """
-    Search for articles that are similar to the bibcodes provided. 
+    Search for articles that are similar to the bibcodes provided.
     """
     bibcodes = list(set(bibcodes))
     search_kwds = dict(rows=rows)
-
-    
-
 
 
 if __name__ == "__main__":
@@ -122,4 +130,4 @@ if __name__ == "__main__":
 
     for i, suggestion in enumerate(name_generator(author_name)):
         print(i, suggestion)
-            
+
