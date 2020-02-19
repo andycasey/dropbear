8460bad598f9229b22b57a64bffa03cbb7f25574
Dan F-M <foreman.mackey@gmail.com>
2020-02-19 16:33:25 +1100

    files for frontend

---
+++ simple.py
@@ -0,0 +1,30 @@
+from aiohttp import web
+import jinja2
+import aiohttp_jinja2
+
+
+async def stream(request):
+    response = web.StreamResponse(
+        status=200, reason="OK", headers={"Content-Type": "text/plain"},
+    )
+    await response.prepare(request)
+
+    for i in range(5000):
+        await response.write("{0}\n".format(i).encode("utf-8"))
+
+    await response.write_eof()
+    return response
+
+
+@aiohttp_jinja2.template("index.html")
+async def index(request):
+    return {}
+
+
+app = web.Application()
+app.add_routes([web.get("/", index)])
+app.add_routes([web.get("/stream", stream)])
+
+aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader("./templates"))
+
+web.run_app(app)
